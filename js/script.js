fa5b82104f5a3b1001ca0fecfade26cc06b7ec8a
Abigail S <sanders.abigailc@gmail.com>
2024-01-30 00:26:10 -0600

    Added food assignments

--- js/script.js
+++ js/script.js
@@ -10,3 +10,75 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+const assignButton = document.querySelector(".assign");
+const assignedItems = document.querySelector(".assigned-items");
+
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    //console.log(guest);
+    if (guest !== "") {
+        addToList(guest);
+        updateGuestCount();
+        clearInput();
+    }
+});
+
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const clearInput = function () {
+    guestInput.value = "";
+};
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+};
+
+const assignItems = function () {
+    const potluckItems = [
+        "Cookies",
+        "Mashed Potatoes",
+        "Sweet Potato Casserole",
+        "Rolls",
+        "Three Bean Salad",
+        "Watergate Salad",
+        "Turkey",
+        "Ham",
+        "Cranberry Sauce",
+        "Gravy",
+        "Apple Pie",
+        "Macaroni and Cheese"
+    ];
+
+    const allGuests = document.querySelectorAll(".guest-list li");
+
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}.`;
+        assignedItems.append(listItem);
+
+        potluckItems.splice(randomPotluckIndex, 1);
+
+    }
+};
+
+assignButton.addEventListener("click", function () {
+    assignItems();
+    assignButton.disabled = true;
+});
